f3d99f2e964817a722be4879b8661c0b5f6890a6
kyle shuttleworth <shuttleworth0428@yahoo.com>
2019-07-04 22:33:13 -0400

    add more data to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (latitude, longitude, callback) =>{
         callback('unable to fetch the location entered')
     }else {
         callback(undefined,
-            `${body.daily.data[0].summary} It is currently ${body.currently.temperature} degrees out. There is a ${body.currently.precipProbability}% chance of rain`
+            `${body.daily.data[0].summary} It is currently ${body.currently.temperature} degrees out. The high for today is ${body.daily.data[0].temperatureHigh} degrees and the low is ${body.daily.data[0].temperatureLow} degrees. There is a ${body.currently.precipProbability}% chance of rain`
          )
     }
    })
